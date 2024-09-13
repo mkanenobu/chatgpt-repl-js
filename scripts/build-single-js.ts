b1166c8d51a90f30fc41ea119b595300c441d687
mkanenobu <mitsuru.kanenobu@gmail.com>
2024-09-13 18:13:28 +0900

    fix build

--- scripts/build-single-js.ts
+++ scripts/build-single-js.ts
@@ -1,13 +1,22 @@
 import { build } from "esbuild";
+import { chmod } from "node:fs/promises";
 
 const main = async () => {
+  const outfile = "out/chatgpt-repl.cjs";
+
   const result = await build({
     bundle: true,
     platform: "node",
-    outfile: "out/bundle.js",
+    format: "cjs",
+    outfile,
     entryPoints: ["main.ts"],
+    banner: {
+      js: "#!/usr/bin/env node --no-warnings=ExperimentalWarning",
+    },
   });
   console.log(result);
+
+  chmod(outfile, 0o755);
 };
 
 await main();
